f68e4a6e7db6ce6c9888e025241f378504fe8d0a
Gabril <62163345+Gabril0@users.noreply.github.com>
2023-01-31 23:04:28 -0300

    Update ContaCorrente.java

--- ContaCorrente.java
+++ ContaCorrente.java
@@ -41,13 +41,17 @@ public class ContaCorrente extends Conta{
         if(getSaldo()<valor){throw new SaldoInsuficienteException("Erro!! Saldo insuficiente!");}
         else {
             valorAux = getSaldo();
-            setSaldo(getSaldo()-valor);}
+            setSaldo(getSaldo()-valor);
+            Transacao t = new Transacao(this, getDataAtual(), "Caixa eletronico");
+            System.out.println(t.printRecibo());}
     }
 
     public void deposito(double valor){
         if(valor>0 && getSaldo()+(valor*taxaParaCalculo) <= limite){
         valorAux = getSaldo();
-        setSaldo(getSaldo()+(valor*taxaParaCalculo));}
+        setSaldo(getSaldo()+(valor*taxaParaCalculo));
+        Transacao t = new Transacao(this, getDataAtual(), "Digital");
+        System.out.println(t.printRecibo());}
         else{throw new ValorMenorQue0Exception("Erro!! O valor deve ser maior que 0 ou menor que o limite");}
     }
 
@@ -61,6 +65,8 @@ public class ContaCorrente extends Conta{
             valorAux = getSaldo();
             setSaldo(getSaldo() - valor);
             destino.setSaldo(destino.getSaldo()+(valor*taxaParaCalculo));
+            Transacao t = new Transacao(this, getDataAtual(), "Digital");
+            System.out.println(t.printRecibo());
         }
     }
 
